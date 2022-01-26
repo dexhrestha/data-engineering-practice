36107c871a98dad370a8dbd8c8f7490a67640f2b
dexhrestha <dipesh.shrestha009@gmail.com>
2022-01-26 23:38:57 +0545

    Completed ingestion of data with docker and postgresql \n Started terraform and gcp

--- Week 1/pipeline/ingest-data.py
+++ Week 1/pipeline/ingest-data.py
@@ -10,26 +10,34 @@ import os
 # !wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv
 #www1.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records
 
+def download(url,output):
+    if not os.path.exists(output):
+        with requests.get(url,stream=True) as r:
+            r.raise_for_status()
+            with open(output,'wb')  as f:
+                for chunk in r.iter_content(chunk_size=8000):
+                    f.write(chunk)
+        
+        return output
+
 def main(params):
     user = params.user
     password = params.password
     host = params.host
     port = params.port
     db = params.db
-    table_name = params.table_name
-    url = params.url
+    taxi_table_name = params.taxi_table_name
+    zones_table_name = params.zones_table_name
+    source_url = params.source_url
+    zones_url = params.zones_url
 
-    csv_name = 'output.csv'
+    taxi_csv = 'taxi.csv'
+    zones_csv = 'zones.csv'
 
-    #download
-    if not os.path.exists(csv_name):
-        with requests.get(url,stream=True) as r:
-            r.raise_for_status()
-            with open(csv_name,'wb')  as f:
-                for chunk in r.iter_content(chunk_size=8000):
-                    f.write(chunk)
+    # download(source_url,taxi_csv)
+    # download(zones_url,zones_csv)
 
-    df  = pd.read_csv(csv_name,nrows=100) 
+    df  = pd.read_csv(taxi_csv,nrows=100) 
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -40,29 +48,30 @@ def main(params):
 
 
     #Create schema
-    print(pd.io.sql.get_schema(df,name=table_name,con=engine))
+    print(pd.io.sql.get_schema(df,name=taxi_table_name,con=engine))
 
     #Read csv data in chunks using interator
-    df_iter = pd.read_csv(csv_name,iterator=True,chunksize=100000)
+    df_iter = pd.read_csv(taxi_csv,iterator=True,chunksize=100000)
 
 
-    start = time()
+ 
     c = 0
     while True:
+        start = time()
         try:
             df = next(df_iter)
         except:
             break
         df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
         df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-        df.to_sql(name=table_name,con=engine,if_exists="append")
+        df.to_sql(name=taxi_table_name,con=engine,if_exists="append")
         total = time() - start
         print(f"Successfully inserted {c+1} chunks in {total:.2f} seconds")
         c+=1
     
-    zones  = pd.read_csv('../datasets/taxi+_zone_lookup.csv',nrows=100) 
-    print(pd.io.sql.get_schema(zones,name='zones',con=engine))
-    zones.to_sql(name='zones',con=engine,if_exists="replace")
+    zones  = pd.read_csv(zones_csv,nrows=100) 
+    print(pd.io.sql.get_schema(zones,name=zones_table_name,con=engine))
+    zones.to_sql(name=zones_table_name,con=engine,if_exists="replace")
 
 if __name__ == "__main__":
     
@@ -82,8 +91,10 @@ if __name__ == "__main__":
     parser.add_argument('--host',help='host for postgres')
     parser.add_argument('--port',help='port for postgres')
     parser.add_argument('--db',help='database name for postgres')
-    parser.add_argument('--table_name',help='name of table where we will write the results')
-    parser.add_argument('--url',help='url of csv file')
+    parser.add_argument('--taxi_table_name',help='name of table where we will write the taxi results')
+    parser.add_argument('--zones_table_name',help='name of table where we will write the zones results')
+    parser.add_argument('--source_url',help='url of taxi data csv file')
+    parser.add_argument('--zones_url',help='urk of zones data csv file')
 
     main(parser.parse_args())
 
