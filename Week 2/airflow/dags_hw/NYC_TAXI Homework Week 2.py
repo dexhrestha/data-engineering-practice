0753a6c8ea551e2fd4b82105e90fdc2a983a42ca
dexhrestha <dipesh.shrestha009@gmail.com>
2022-02-05 02:40:20 +0545

    Week 2 homework

--- Week 2/airflow/dags_hw/NYC_TAXI Homework Week 2.py
+++ Week 2/airflow/dags_hw/NYC_TAXI Homework Week 2.py
@@ -73,7 +73,7 @@ default_args = {
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
-    dag_id="data_ingestion_gcs_dag",
+    dag_id="nyc_taxi_data_ingestion_gcs_dag",
     schedule_interval="@monthly",
     default_args=default_args,
     catchup=True,
